77334b6b979c8507f4f65793438b56837f96d4c1
Zeno <57388886+ZenoABC@users.noreply.github.com>
2023-12-23 12:53:15 +0300

    i am dum

--- alei.user.js
+++ alei.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ALE Improvements
-// @version      6.5
+// @version      6.6
 // @description  Changes to make ALE better.
 // @author       mici1234, wanted2001, gcp5o
 // @match        *://www.plazmaburst2.com/level_editor/map_edit.php*
@@ -1586,7 +1586,7 @@ function patchUpdateGUIParams() {
         window.GenParamVal = function(base, value) {
             let resp = origGPV(base, value);
 
-            if (FORCE_TEXT_OPTIONS || ["nochange"].indexOf(base) != -1) {
+            if (base == "nochange") {
                 resp = `${value}`.replaceAll('"', "&quot;");
                 resp = `<pvalue real="${resp}">- not used -</pvalue>`
             }
