e07beff9c85fb493c0f34296129239e8e3f77011
Zeno <57388886+ZenoABC@users.noreply.github.com>
2024-01-08 17:50:27 +0300

    bump version

--- alei.user.js
+++ alei.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ALE Improvements
-// @version      8.2
+// @version      8.3
 // @description  Changes to make ALE better.
 // @author       mici1234, wanted2001, gcp5o
 // @match        *://www.plazmaburst2.com/level_editor/map_edit.php*
