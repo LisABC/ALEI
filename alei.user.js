57b0df95120672c5203489b113c19c53ef21dabd
User475186 <150513019+User475186@users.noreply.github.com>
2024-01-20 18:14:56 +0300

    Update alei.user.js

--- alei.user.js
+++ alei.user.js
@@ -1243,6 +1243,96 @@ function patchSaveMap() {
 	window.SaveThisMap = SaveThisMap;
 }
 
+function getRuleBySelector(selector) {
+	let rules = document.styleSheets[0].rules;
+	let rule;
+	
+	for (let i = 0; i < rules.length; i++) {
+		if (rules[i].selectorText == selector) {
+			rule = rules[i];
+		}
+	}
+	
+	return rule;
+}
+
+function setStyle(selector, style, value) {
+	getRuleBySelector(selector).style[style] = value;
+}
+
+function blackTheme() {
+	ThemeSet(0);
+	
+	THEME = 4;
+	
+	setStyle(".topui", "backgroundSize", "0px");
+	setStyle(".leftui", "backgroundSize", "0px");
+	setStyle(".rightui", "backgroundSize", "0px");
+	setStyle(".topui", "backgroundColor", "#101010");
+	setStyle(".leftui", "backgroundColor", "#101010");
+	setStyle(".rightui", "backgroundColor", "#101010");
+	setStyle(".field_btn", "backgroundColor", "#202020");
+	setStyle(".tool_btn", "backgroundColor", "#202020");
+	setStyle(".tool_btn", "border", "");
+	setStyle(".tool_btn2", "backgroundColor", "#333");
+	setStyle(".tool_btn2", "border", "1px solid #888");
+	setStyle(".field_btn", "color", "#CCC");
+	setStyle(".tool_btn", "color", "#CCC");
+	setStyle(".tool_btn2", "color", "#CCC");
+	setStyle(".gui_sel_info", "color", "#CCC");
+	setStyle(".c", "color", "#CCC");
+	setStyle(".pa1", "color", "#CCC");
+	setStyle(".pa2", "color", "#CCC");
+	setStyle(".field_dis_left", "color", "#CCC");
+	setStyle(".field_dis_right", "color", "#CCC");
+	setStyle(".p_u1", "border", "");
+	setStyle(".p_u2", "border", "");
+	setStyle(".pa1", "backgroundColor", "#202020");
+	setStyle(".pa2", "backgroundColor", "#000000");
+	setStyle(".objbox", "backgroundColor", "#000000");
+	setStyle(".field_dis_left", "backgroundColor", "#202020");
+	setStyle(".field_dis_right", "backgroundColor", "#000000");
+	setStyle(".selline1", "backgroundColor", "#202020");
+	setStyle(".tool_btn:hover", "backgroundColor", "#202020");
+	setStyle(".tool_btn:hover", "border", "1px solid #888");
+	setStyle(".tool_btn:hover", "color", "#CCC");
+	setStyle(".tool_btn2:hover", "backgroundColor", "#333");
+	setStyle(".tool_btn2:hover", "border", "1px solid #888");
+	setStyle(".tool_btn2:hover", "color", "#CCC");
+	setStyle(".field_btn:hover", "backgroundColor", "#333");
+	setStyle(".field_btn:hover", "color", "#CCC");
+	setStyle(".tool_btn:active", "backgroundColor", "#202020");
+	setStyle(".tool_btn:active", "border", "1px solid #888");
+	setStyle(".tool_btn:active", "color", "#CCC");
+	setStyle(".tool_btn2:active", "backgroundColor", "#333");
+	setStyle(".tool_btn2:active", "border", "1px solid #888");
+	setStyle(".tool_btn2:active", "color", "#CCC");
+	setStyle(".field_btn:active", "backgroundColor", "#333");
+	setStyle(".field_btn:active", "color", "#CCC");
+	setStyle("#mrtitle", "backgroundColor", "#202020");
+	setStyle("#mrbox", "backgroundColor", "#101010");
+	setStyle(".field_input", "backgroundColor", "#000");
+	setStyle(".field_input", "color", "#CCC");
+	setStyle(".btn", "backgroundColor", "#202020");
+	setStyle(".btn", "color", "#CCC");
+	setStyle(".btn:hover", "backgroundColor", "#333");
+	setStyle(".btn:hover", "color", "#CCC");
+	setStyle(".btn:active", "backgroundColor", "#333");
+	setStyle(".btn:active", "color", "#CCC");
+	setStyle("closebox", "backgroundColor", "#101010");
+	setStyle("closebox", "color", "#CCC");
+	setStyle(".list_group", "backgroundColor", "#202020");
+	setStyle(".list_group", "borderBottom", "");
+	setStyle(".list_group:hover", "backgroundColor", "#333");
+	setStyle(".list_group:active", "backgroundColor", "#333");
+	setStyle(".image_list_collapsable", "backgroundColor", "#101010");
+	setStyle(".img_option_selected", "backgroundColor", "#444");
+	setStyle(".rightui", "borderLeft", "");
+	setStyle("::-webkit-scrollbar-thumb", "backgroundColor", "#888");
+	setStyle("#tools_box", "overflow-y", "hidden");
+	setStyle("#tools_box", "overflow-y", "auto");
+}
+
 document.addEventListener("keydown", e => {
     if (e.ctrlKey && e.code == "KeyS") {
         e.preventDefault();
@@ -2200,6 +2290,12 @@ let ALE_start = (async function() {
 	patchSaveMap();
     NewNote("ALEI: Welcome!", "#7777FF");
     aleiLog(INFO, "Welcome!")
+	
+	if (aleiSettings.blackTheme) {
+		blackTheme();
+	}
+	
+	UpdateTools();
 });
 
 document.addEventListener("DOMContentLoaded", () => ALE_start());
