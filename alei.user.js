203eb5d193bb14b9a12ff059f99f9088d88dd308
Lisa <57388886+LisABC@users.noreply.github.com>
2024-02-16 23:39:35 +0300

    bump version

--- alei.user.js
+++ alei.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ALE Improvements
-// @version      11
+// @version      11.1
 // @description  Changes to make ALE better.
 // @author       mici1234, wanted2001, gcp5o
 // @match        *://www.plazmaburst2.com/level_editor/map_edit.php*
