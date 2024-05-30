d6af861a15d90b353afb655c753990dc9db4ddc6
Lisa <57388886+LisABC@users.noreply.github.com>
2024-05-30 22:50:09 +0300

    Forgot to account for trigger action selection

--- alei.user.js
+++ alei.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ALE Improvements
-// @version      12.7
+// @version      12.8
 // @description  Changes to make ALE better.
 // @author       mici1234, wanted2001, gcp5o
 // @match        *://www.plazmaburst2.com/level_editor/map_edit.php*
@@ -3854,10 +3854,10 @@ let triggerActionsClipboard = [];
 function getTriggerActionElements() {
     let arr = [];
     let elems = document.getElementsByClassName("p_i");
-    let i = 5;
+    let i = 6;
 
     if (aleiSettings.showIDs) {
-        i = 6;
+        i++;
     }
 
     for (; i < elems.length; i++) {
