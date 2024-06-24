e4122198ec98c8e2a8bd7b124336cc15bfdce74b
Lisa <57388886+LisABC@users.noreply.github.com>
2024-06-24 16:01:03 +0300

    uncomment check updates

--- alei.user.js
+++ alei.user.js
@@ -5209,7 +5209,7 @@ let ALE_start = (async function() {
     addFunctionToWindow();
 
     if(isNative) {
-        //checkForUpdates();
+        checkForUpdates();
         changeTopRightText();
     } else {
         // load this map twice to parse extended triggers.
