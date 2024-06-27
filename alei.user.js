3c7067266449959f95e91e05d624f17898a85bff
Lisa <57388886+LisABC@users.noreply.github.com>
2024-06-27 17:09:42 +0300

    Make ctrl + shift only be in effect ONCE

--- alei.user.js
+++ alei.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ALE Improvements
-// @version      13.9
+// @version      14.0
 // @description  Changes to make ALE better.
 // @author       mici1234, wanted2001, gcp5o
 // @match        *://www.plazmaburst2.com/level_editor/map_edit.php*
@@ -2804,7 +2804,10 @@ document.addEventListener("keydown", e => {
     }
 
     if (e.ctrlKey && e.shiftKey) {
-        if (newUpdate) window.open("https://github.com/LisABC/ALEI/raw/main/alei.user.js");
+        if (newUpdate) {
+            window.open("https://github.com/LisABC/ALEI/raw/main/alei.user.js");
+            newUpdate = false;
+        }
     }
 });
 
