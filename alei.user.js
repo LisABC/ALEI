3bfa7a69b2d3e10532de21207b363278889f118f
Lisa <57388886+LisABC@users.noreply.github.com>
2024-06-28 15:39:10 +0300

    Reset OCM on start as to prevent error in empty map

--- alei.user.js
+++ alei.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ALE Improvements
-// @version      14.8
+// @version      14.9
 // @description  Changes to make ALE better.
 // @author       mici1234, wanted2001, gcp5o
 // @match        *://www.plazmaburst2.com/level_editor/map_edit.php*
@@ -5419,6 +5419,8 @@ let ALE_start = (async function() {
     ROOT_ELEMENT = document.documentElement;
     stylesheets = document.styleSheets;
     ALE_Render = Render;
+    window.ObjectConnectionMapping = {};
+    window.uidToElementMap = {};
 
     // Updates the current CSS stylesheet.
     updateCSSFile();
