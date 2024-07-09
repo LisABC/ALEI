4e42074ef9a909936f93477dc54d1b875d6d1ecc
Lisa <57388886+LisABC@users.noreply.github.com>
2024-07-09 18:49:30 +0300

    Prevent setting field if it errors

--- alei.user.js
+++ alei.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ALE Improvements
-// @version      16.1
+// @version      16.2
 // @description  Changes to make ALE better.
 // @author       mici1234, wanted2001, gcp5o
 // @match        *://www.plazmaburst2.com/level_editor/map_edit.php*
@@ -3067,6 +3067,7 @@ function setParameter(index, value) {
         i++;
         actualIndex++;
 
+        if(rightParams.childNodes[actualIndex] == undefined) continue;
         if(rightParams.childNodes[actualIndex].childNodes[1] === undefined) actualIndex++;
     }
 
